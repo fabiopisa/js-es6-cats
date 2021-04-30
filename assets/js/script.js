2392920e0730aa6460263598cb59afcd6b58c0f6
fabiopisa <pisanellifabio@gmail.com>
2021-04-30 14:48:09 +0200

    secondo commit esercizio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 /* Definire un array di oggetti; ogni oggetto rappresenta un gatto, che è caratterizzato da: nome, età, colore e sesso.
 
+Milestone 1
 Tramite la funzione .forEach(), stampare in pagina tutti i gattini, ciascuno con il proprio colore e il proprio nome.
 
 Milestone 2
@@ -42,4 +43,54 @@ $(document).ready(function(){
     },
   ];
 
+  //Milestone 1
+  arrCats.forEach((cat) => {
+     $('#mailes-1 ul').append(listGenerator(cat.color, cat.name))
+  })
+  //end Milestone 1
+
+  //Milestone 2
+  const pink = '#FF00E6';
+  const blue = '#0084FF';
+  const arrNewCats = arrCats.map((cat) => {
+    let color = (cat.gender === 'female') ? pink : blue ;
+    let opacity = cat.age / 10;
+    return {
+      ...cat,
+      ribbon: {
+        color,
+        opacity
+      }
+    }
+  })//end map
+  
+
+  const femaleCats = arrNewCats.filter((cat) => cat.gender === 'female');
+  const maleCats = arrNewCats.filter((cat) => cat.gender === 'male');
+
+  femaleCats.forEach((cat) => {
+    $('#mailes-2-female ul').append(listGenerator(cat.color, cat.name, cat.ribbon.color,cat.ribbon.opacity))
+  })
+  maleCats.forEach((cat) => {
+    $('#mailes-2-male ul').append(listGenerator(cat.color, cat.name, cat.ribbon.color,cat.ribbon.opacity))
+  })
+
+  //end Milestone 2
+
 });
+
+//funzioni 
+function listGenerator (colorCat, name, ...ribbon){
+  let tagRibbon ="";
+  if(ribbon.length > 0){
+    tagRibbon = `<i class="fas fa-ribbon" style="color:${ribbon[0]}; opacity:${ribbon[1]};"></i>`
+  }
+  let html = `
+    <li>
+      <i class="fas fa-cat" style="color:${colorCat}"></i>
+      ${tagRibbon}
+      <span>${name}</span>
+    </li>
+  `
+  return html
+}
